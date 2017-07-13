16ac87d9ceea159b84555fdd716641b611338b38
Alexey Golub <Tyrrrrrr@gmail.com>
2017-07-13 20:34:16 +0300

    Refactor

---
+++ DiscordChatExporter/Models/MessageGroup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiscordChatExporter.Models
+{
+    public class MessageGroup
+    {
+        public MessageGroup(User author, DateTime firstTimeStamp, IEnumerable<Message> messages)
+        {
+            Author = author;
+            FirstTimeStamp = firstTimeStamp;
+            Messages = messages.ToArray();
+        }
+
+        public User Author { get; }
+
+        public DateTime FirstTimeStamp { get; }
+
+        public IReadOnlyList<Message> Messages { get; }
+    }
+}
--- DiscordChatExporter/Services/ExportService.cs
+++ DiscordChatExporter/Services/ExportService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
@@ -11,22 +10,6 @@ namespace DiscordChatExporter.Services
 {
     public class ExportService
     {
-        private class MessageGroup
-        {
-            public User Author { get; }
-
-            public DateTime FirstTimeStamp { get; }
-
-            public IReadOnlyList<Message> Messages { get; }
-
-            public MessageGroup(User author, DateTime firstTimeStamp, IEnumerable<Message> messages)
-            {
-                Author = author;
-                FirstTimeStamp = firstTimeStamp;
-                Messages = messages.ToArray();
-            }
-        }
-
         private HtmlDocument GetTemplate()
         {
             const string templateName = "DiscordChatExporter.Services.ExportTemplate.html";

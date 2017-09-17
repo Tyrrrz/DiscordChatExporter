a37c7ae160d52774ccb1e4e76c4e83163b0ba4cc
Alexey Golub <Tyrrrrrr@gmail.com>
2017-09-17 17:24:49 +0300

    Basic layout

---
+++ DiscordChatExporter/Views/ExportDialog.ammy.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscordChatExporter.Views
+{
+    public partial class ExportDialog
+    {
+        public ExportDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}

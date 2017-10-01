b280f8b73689176c775ff33d53975cd4c7d22dc4
Alexey Golub <Tyrrrrrr@gmail.com>
2017-10-01 11:27:21 +0300

    Add AM/PM to default date format as it was previously missing it

--- DiscordChatExporter/Services/SettingsService.cs
+++ DiscordChatExporter/Services/SettingsService.cs
@@ -6,7 +6,7 @@ namespace DiscordChatExporter.Services
     public class SettingsService : SettingsManager, ISettingsService
     {
         public Theme Theme { get; set; }
-        public string DateFormat { get; set; } = "dd-MMM-yy hh:mm";
+        public string DateFormat { get; set; } = "dd-MMM-yy hh:mm tt";
         public int MessageGroupLimit { get; set; } = 20;
 
         public string LastToken { get; set; }

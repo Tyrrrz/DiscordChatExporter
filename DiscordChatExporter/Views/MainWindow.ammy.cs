670982132460f173b28bdf42471c2dd66f2a586d
Alexey Golub <Tyrrrrrr@gmail.com>
2017-10-06 23:27:17 +0300

    Cleanup views

--- DiscordChatExporter/Views/MainWindow.ammy.cs
+++ DiscordChatExporter/Views/MainWindow.ammy.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Windows.Input;
 using DiscordChatExporter.Messages;
-using DiscordChatExporter.ViewModels;
 using GalaSoft.MvvmLight.Messaging;
 using MaterialDesignThemes.Wpf;
 using Tyrrrz.Extensions;
@@ -10,26 +8,20 @@ namespace DiscordChatExporter.Views
 {
     public partial class MainWindow
     {
-        private IMainViewModel ViewModel => (IMainViewModel) DataContext;
-
         public MainWindow()
         {
             InitializeComponent();
             Title += $" v{Assembly.GetExecutingAssembly().GetName().Version}";
 
-            Messenger.Default.Register<ShowErrorMessage>(this, m => DialogHost.Show(new ErrorDialog()).Forget());
-            Messenger.Default.Register<ShowExportDoneMessage>(this, m => DialogHost.Show(new ExportDoneDialog()).Forget());
-            Messenger.Default.Register<ShowExportSetupMessage>(this, m => DialogHost.Show(new ExportSetupDialog()).Forget());
-            Messenger.Default.Register<ShowSettingsMessage>(this, m => DialogHost.Show(new SettingsDialog()).Forget());
-        }
-
-        public void TokenTextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                // Execute command
-                ViewModel.PullDataCommand.Execute(null);
-            }
+            // Dialogs
+            Messenger.Default.Register<ShowErrorMessage>(this,
+                m => DialogHost.Show(new ErrorDialog()).Forget());
+            Messenger.Default.Register<ShowExportDoneMessage>(this,
+                m => DialogHost.Show(new ExportDoneDialog()).Forget());
+            Messenger.Default.Register<ShowExportSetupMessage>(this,
+                m => DialogHost.Show(new ExportSetupDialog()).Forget());
+            Messenger.Default.Register<ShowSettingsMessage>(this,
+                m => DialogHost.Show(new SettingsDialog()).Forget());
         }
     }
 }

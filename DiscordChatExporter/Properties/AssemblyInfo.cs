5d6edfd93787f82a5c18989a0a1aa6a7a9b55602
Alexey Golub <Tyrrrrrr@gmail.com>
2017-10-27 22:36:25 +0300

    Update version

--- DiscordChatExporter/Properties/AssemblyInfo.cs
+++ DiscordChatExporter/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("DiscordChatExporter")]
 [assembly: AssemblyCompany("Tyrrrz")]
 [assembly: AssemblyCopyright("Copyright (c) 2017 Alexey Golub")]
-[assembly: AssemblyVersion("2.2.1")]
-[assembly: AssemblyFileVersion("2.2.1")]
+[assembly: AssemblyVersion("2.3")]
+[assembly: AssemblyFileVersion("2.3")]

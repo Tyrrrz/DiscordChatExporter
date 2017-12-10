c432a75f62fb1c7cdf2b0afe6e005765436bb575
Alexey Golub <Tyrrrrrr@gmail.com>
2017-12-10 22:36:16 +0200

    Simplify deployment script

--- Deploy/Prepare.ps1
+++ Deploy/Prepare.ps1
@@ -1,22 +1,3 @@
-$path = "$PSScriptRoot\..\DiscordChatExporter\bin\Release\*"
-$include = "*.exe", "*.dll", "*.config"
-$outputDir = "$PSScriptRoot\Output"
-$outputFile = "DiscordChatExporter.zip"
-
-# Create output directory
-if (-Not (Test-Path $outputDir))
-{
-    New-Item $outputDir -ItemType Directory
-}
-
-# Delete output if already exists
-if (Test-Path("$outputDir\$outputFile"))
-{
-    Remove-Item -Path "$outputDir\$outputFile"
-}
-
-# Get files
-$files = Get-ChildItem -Path $path -Include $include
-
-# Pack into archive
-$files | Compress-Archive -DestinationPath "$outputDir\$outputFile"
+New-Item "$PSScriptRoot\bin" -ItemType Directory -Force
+$files = Get-ChildItem -Path "$PSScriptRoot\..\DiscordChatExporter\bin\Release\*" -Include "*.exe", "*.dll", "*.config"
+$files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\DiscordChatExporter.zip" -Force

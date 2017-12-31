6ccba438075f1793239cecfd20042e6f64c24933
Alexey Golub <Tyrrrrrr@gmail.com>
2017-12-31 14:12:35 +0200

    Update copyright

--- DiscordChatExporter/Properties/AssemblyInfo.cs
+++ DiscordChatExporter/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("DiscordChatExporter")]
 [assembly: AssemblyCompany("Tyrrrz")]
-[assembly: AssemblyCopyright("Copyright (c) 2017 Alexey Golub")]
+[assembly: AssemblyCopyright("Copyright (c) 2017-2018 Alexey Golub")]
 [assembly: AssemblyVersion("2.3")]
 [assembly: AssemblyFileVersion("2.3")]

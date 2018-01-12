2094f02f0b2de1221715628364e5f2a6380cc9b1
Alexey Golub <Tyrrrrrr@gmail.com>
2018-01-12 20:42:55 +0200

    Refactor cli a bit

---
+++ DiscordChatExporter.Cli/CliOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DiscordChatExporter.Core.Models;
+
+namespace DiscordChatExporter.Cli
+{
+    public class CliOptions
+    {
+        public string Token { get; set; }
+
+        public string ChannelId { get; set; }
+
+        public ExportFormat ExportFormat { get; set; }
+
+        public string FilePath { get; set; }
+
+        public DateTime? From { get; set; }
+
+        public DateTime? To { get; set; }
+    }
+}
--- DiscordChatExporter.Cli/Program.cs
+++ DiscordChatExporter.Cli/Program.cs
@@ -8,38 +8,69 @@ namespace DiscordChatExporter.Cli
     {
         private static readonly Container Container = new Container();
 
+        private static CliOptions ParseOptions(string[] args)
+        {
+            var argsParser = new FluentCommandLineParser<CliOptions>();
+
+            argsParser.Setup(o => o.Token)
+                .As('t', "token")
+                .Required()
+                .WithDescription("Discord authorization token.");
+
+            argsParser.Setup(o => o.ChannelId)
+                .As('c', "channel")
+                .Required()
+                .WithDescription("Discord channel ID.");
+
+            argsParser.Setup(o => o.ExportFormat)
+                .As('f', "format")
+                .SetDefault(ExportFormat.HtmlDark)
+                .WithDescription("Export format (PlainText/HtmlDark/HtmlLight). Optional.");
+
+            argsParser.Setup(o => o.FilePath)
+                .As('o', "output")
+                .SetDefault(null)
+                .WithDescription("Output file path. Optional.");
+
+            argsParser.Setup(o => o.From)
+                .As("datefrom")
+                .SetDefault(null)
+                .WithDescription("Messages after this date. Optional.");
+
+            argsParser.Setup(o => o.To)
+                .As("dateto")
+                .SetDefault(null)
+                .WithDescription("Messages before this date. Optional.");
+
+            var parsed = argsParser.Parse(args);
+
+            // Show help if no arguments
+            if (parsed.EmptyArgs)
+            {
+                Console.WriteLine("=== Discord Chat Exporter (Command Line Interface) ===");
+                Console.WriteLine();
+                Console.WriteLine("[-t] [--token]     Discord authorization token.");
+                Console.WriteLine("[-c] [--channel]   Discord channel ID.");
+                Console.WriteLine("[-f] [--format]    Export format (PlainText/HtmlDark/HtmlLight). Optional.");
+                Console.WriteLine("[-o] [--output]    Output file path. Optional.");
+                Console.WriteLine("     [--datefrom]  Limit to messages after this date. Optional.");
+                Console.WriteLine("     [--dateto]    Limit to messages before this date. Optional.");
+                Environment.Exit(0);
+            }
+            // Show error if there are any
+            else if (parsed.HasErrors)
+            {
+                Console.Error.Write(parsed.ErrorText);
+                Environment.Exit(-1);
+            }
+
+            return argsParser.Object;
+        }
+
         public static void Main(string[] args)
         {
-            // Parse args
-            var argsParser = new FluentCommandLineParser();
-
-            string token = null;
-            argsParser.Setup<string>('t', "token")
-                .Callback(v => token = v)
-                .Required();
-
-            string channelId = null;
-            argsParser.Setup<string>('c', "channel")
-                .Callback(v => channelId = v)
-                .Required();
-
-            var exportFormat = ExportFormat.HtmlDark;
-            argsParser.Setup<ExportFormat>('f', "format")
-                .Callback(v => exportFormat = v);
-
-            string filePath = null;
-            argsParser.Setup<string>('o', "output")
-                .Callback(v => filePath = v);
-
-            DateTime? from = null;
-            argsParser.Setup<DateTime>("datefrom")
-                .Callback(v => from = v);
-
-            DateTime? to = null;
-            argsParser.Setup<DateTime>("dateto")
-                .Callback(v => to = v);
-
-            argsParser.Parse(args);
+            // Parse options
+            var options = ParseOptions(args);
 
             // Init container
             Container.Init();
@@ -48,7 +79,13 @@ namespace DiscordChatExporter.Cli
             var vm = Container.MainViewModel;
 
             // Export
-            vm.ExportAsync(token, channelId, filePath, exportFormat, from, to).GetAwaiter().GetResult();
+            vm.ExportAsync(
+                options.Token,
+                options.ChannelId,
+                options.FilePath,
+                options.ExportFormat,
+                options.From,
+                options.To).GetAwaiter().GetResult();
 
             // Cleanup container
             Container.Cleanup();

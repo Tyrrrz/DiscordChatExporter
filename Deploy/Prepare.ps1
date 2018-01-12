8fdffcad2e697e9a80499d716261b3c4d769875f
Alexey Golub <Tyrrrrrr@gmail.com>
2018-01-12 21:18:18 +0200

    Fix deploy script

--- Deploy/Prepare.ps1
+++ Deploy/Prepare.ps1
@@ -1,5 +1,5 @@
 New-Item "$PSScriptRoot\bin" -ItemType Directory -Force
 $files = @()
 $files += Get-ChildItem -Path "$PSScriptRoot\..\DiscordChatExporter.Gui\bin\Release\*" -Include "*.exe", "*.dll", "*.config"
-$files += Get-ChildItem -Path "$PSScriptRoot\..\DiscordChatExporter.Cli\bin\Release\*" -Include "*.exe", "*.dll", "*.config"
+$files += Get-ChildItem -Path "$PSScriptRoot\..\DiscordChatExporter.Cli\bin\Release\net45\*" -Include "*.exe", "*.dll", "*.config"
 $files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\DiscordChatExporter.zip" -Force

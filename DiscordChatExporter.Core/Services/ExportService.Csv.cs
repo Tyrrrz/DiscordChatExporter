2548d6f12baa256b71bd58ff7c57d92bb0a87895
Tim van den Essen <tim3345@live.nl>
2018-04-14 19:46:24 +0200

    add csv exporter and implement it

---
+++ DiscordChatExporter.Core/Services/ExportService.Csv.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using DiscordChatExporter.Core.Models;
+using Tyrrrz.Extensions;
+
+namespace DiscordChatExporter.Core.Services
+{
+    public partial class ExportService
+    {
+        private string FormatMessageContentCsv(Message message)
+        {
+            var content = message.Content;
+
+            // New lines
+            content = content.Replace("\n", "\\n");
+
+            // User mentions (<@id> and <@!id>)
+            foreach (var mentionedUser in message.MentionedUsers)
+                content = Regex.Replace(content, $"<@!?{mentionedUser.Id}>", $"@{mentionedUser}");
+
+            // Role mentions (<@&id>)
+            foreach (var mentionedRole in message.MentionedRoles)
+                content = content.Replace($"<@&{mentionedRole.Id}>", $"@{mentionedRole.Name}");
+
+            // Channel mentions (<#id>)
+            foreach (var mentionedChannel in message.MentionedChannels)
+                content = content.Replace($"<#{mentionedChannel.Id}>", $"#{mentionedChannel.Name}");
+
+            // Custom emojis (<:name:id>)
+            content = Regex.Replace(content, "<(:.*?:)\\d*>", "$1");
+
+            return content;
+        }
+
+        private async Task ExportAsCsvAsync(ChannelChatLog log, TextWriter output)
+        {
+            using (var writer = new CsvHelper.CsvWriter(output, false))
+            {
+                // Chat log
+                foreach (var group in log.MessageGroups)
+                {
+                    foreach (var msg in group.Messages)
+                    {
+                        writer.WriteField(msg.Author.FullName);
+                        var timeStampFormatted = msg.TimeStamp.ToString(_settingsService.DateFormat);
+                        writer.WriteField(timeStampFormatted);
+
+                        // Content
+                        writer.WriteField(string.IsNullOrWhiteSpace(msg.Content) ? null : FormatMessageContentCsv(msg), true);
+
+                        // Attachments
+                        writer.WriteField(msg.Attachments.Select(x => x.Url).JoinToString(","), true);
+
+                        writer.NextRecord();
+                    }
+                }
+            }
+        }
+    }
+}
--- DiscordChatExporter.Core/Services/ExportService.cs
+++ DiscordChatExporter.Core/Services/ExportService.cs
@@ -40,6 +40,10 @@ namespace DiscordChatExporter.Core.Services
                         .GetManifestResourceString("DiscordChatExporter.Core.Resources.ExportService.LightTheme.css");
                     await ExportAsHtmlAsync(log, output, css);
                 }
+                else if (format == ExportFormat.Csv)
+                {
+                    await ExportAsCsvAsync(log, output);
+                }
 
                 else throw new ArgumentOutOfRangeException(nameof(format));
             }

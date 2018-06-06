9477318992c7a4b48ea95e7a7911c494f6f4491f
Oleksii Holub <Oleksii.Holub@3shape.com>
2018-06-06 18:07:52 +0300

    Use template loader

---
+++ DiscordChatExporter.Core/Services/ExportService.TemplateLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using DiscordChatExporter.Core.Internal;
+using DiscordChatExporter.Core.Models;
+using Scriban;
+using Scriban.Parsing;
+using Scriban.Runtime;
+
+namespace DiscordChatExporter.Core.Services
+{
+    public partial class ExportService
+    {
+        public class TemplateLoader : ITemplateLoader
+        {
+            private const string ResourceRootNamespace = "DiscordChatExporter.Core.Resources.ExportTemplates";
+
+            public string GetPath(TemplateContext context, SourceSpan callerSpan, string templateName)
+            {
+                return $"{ResourceRootNamespace}.{templateName}";
+            }
+
+            public string GetPath(ExportFormat format)
+            {
+                return $"{ResourceRootNamespace}.{format}.{format.GetFileExtension()}";
+            }
+
+            public string Load(TemplateContext context, SourceSpan callerSpan, string templatePath)
+            {
+                return Assembly.GetExecutingAssembly().GetManifestResourceString(templatePath);
+            }
+
+            public string Load(ExportFormat format)
+            {
+                return Assembly.GetExecutingAssembly().GetManifestResourceString(GetPath(format));
+            }
+        }
+    }
+}
--- DiscordChatExporter.Core/Services/ExportService.cs
+++ DiscordChatExporter.Core/Services/ExportService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Drawing;
-using System.IO;
-using System.Reflection;
-using DiscordChatExporter.Core.Internal;
+﻿using System.IO;
 using DiscordChatExporter.Core.Models;
 using Scriban;
 using Scriban.Runtime;
-using Scriban.Syntax;
 
 namespace DiscordChatExporter.Core.Services
 {
@@ -19,82 +14,19 @@ namespace DiscordChatExporter.Core.Services
             _settingsService = settingsService;
         }
 
-        private Template GetTemplate(ExportFormat format)
-        {
-            // Resource root namespace for all templates
-            const string resourceRootNamespace = "DiscordChatExporter.Core.Resources.ExportTemplates";
-
-            if (format == ExportFormat.PlainText)
-            {
-                // Get template code
-                var raw = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.PlainText.Template.txt");
-
-                // Parse
-                return Template.Parse(raw);
-            }
-
-            if (format == ExportFormat.HtmlDark)
-            {
-                // Get css
-                var sharedCss = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.Html.Shared.css");
-                var themeCss = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.Html.DarkTheme.css");
-                var css = sharedCss + Environment.NewLine + themeCss;
-
-                // Get template code
-                var raw = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.Html.Template.html");
-                
-                // Inject css
-                raw = raw.Replace("<style></style>", $"<style>{css}</style>");
-
-                // Parse
-                return Template.Parse(raw);
-            }
-
-            if (format == ExportFormat.HtmlLight)
-            {
-                // Get css
-                var sharedCss = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.Html.Shared.css");
-                var themeCss = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.Html.LightTheme.css");
-                var css = sharedCss + Environment.NewLine + themeCss;
-
-                // Get template code
-                var raw = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.Html.Template.html");
-
-                // Inject css
-                raw = raw.Replace("<style></style>", $"<style>{css}</style>");
-
-                // Parse
-                return Template.Parse(raw);
-            }
-
-            if (format == ExportFormat.Csv)
-            {
-                // Get template code
-                var raw = Assembly.GetExecutingAssembly()
-                    .GetManifestResourceString($"{resourceRootNamespace}.Csv.Template.csv");
-
-                // Parse
-                return Template.Parse(raw);
-            }
-
-            throw new ArgumentOutOfRangeException(nameof(format));
-        }
-
         public void Export(ExportFormat format, string filePath, ChannelChatLog log)
         {
+            // Create template loader
+            var loader = new TemplateLoader();
+
             // Get template
-            var template = GetTemplate(format);
+            var templateCode = loader.Load(format);
+            var template = Template.Parse(templateCode);
 
             // Create template context
             var context = new TemplateContext();
             context.MemberRenamer = m => m.Name;
+            context.TemplateLoader = loader;
 
             // Create script object
             var scriptObject = new ScriptObject();

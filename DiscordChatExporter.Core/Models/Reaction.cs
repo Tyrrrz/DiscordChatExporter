e8436faf66dd85472a9cf03eda192323332fcc10
IntriguingTiles <jnickw@outlook.com>
2018-06-25 15:27:45 -0600

    Add support for reactions (#62)

---
+++ DiscordChatExporter.Core/Models/Reaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DiscordChatExporter.Core.Models
+{
+    public class Reaction
+    {
+        public int Count { get; }
+
+        public bool Me { get; }
+
+        public string Id { get; }
+
+        public string Name { get; }
+
+        public string Emoji { get; }
+
+        public Reaction(int count, bool me, string id, string name) 
+        {
+            Count = count;
+            Me = me;
+            Id = id;
+            Name = name;
+            Emoji = (id == "" ? name : $"<:{name}:{id}>");
+        }
+    }
+}

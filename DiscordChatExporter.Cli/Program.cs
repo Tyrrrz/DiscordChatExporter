9a1e732e5c029168105879d66ba10f8a7d943aba
Alexey Golub <Tyrrrrrr@gmail.com>
2018-08-13 22:34:14 +0300

    Add the bot token help back

--- DiscordChatExporter.Cli/Program.cs
+++ DiscordChatExporter.Cli/Program.cs
@@ -1,4 +1,5 @@
-﻿using CommandLine;
+﻿using System;
+using CommandLine;
 using DiscordChatExporter.Cli.Verbs;
 using DiscordChatExporter.Cli.Verbs.Options;
 
@@ -6,6 +7,24 @@ namespace DiscordChatExporter.Cli
 {
     public static class Program
     {
+        private static void ShowTokenHelp()
+        {
+            Console.WriteLine("# To get user token:");
+            Console.WriteLine(" - Open Discord app");
+            Console.WriteLine(" - Log in if you haven't");
+            Console.WriteLine(" - Press Ctrl+Shift+I to show developer tools");
+            Console.WriteLine(" - Navigate to the Application tab");
+            Console.WriteLine(" - Expand Storage > Local Storage > https://discordapp.com");
+            Console.WriteLine(" - Find the \"token\" key and copy its value");
+            Console.WriteLine();
+            Console.WriteLine("# To get bot token:");
+            Console.WriteLine(" - Go to Discord developer portal");
+            Console.WriteLine(" - Log in if you haven't");
+            Console.WriteLine(" - Open your application's settings");
+            Console.WriteLine(" - Navigate to the Bot section on the left");
+            Console.WriteLine(" - Under Token click Copy");
+        }
+
         public static void Main(string[] args)
         {
             // Get all verb types
@@ -27,6 +46,10 @@ namespace DiscordChatExporter.Cli
             parsedArgs.WithParsed<GetDirectMessageChannelsOptions>(o => new GetDirectMessageChannelsVerb(o).Execute());
             parsedArgs.WithParsed<GetGuildsOptions>(o => new GetGuildsVerb(o).Execute());
             parsedArgs.WithParsed<UpdateAppOptions>(o => new UpdateAppVerb(o).Execute());
+
+            // Show token help if error
+            if (parsedArgs.Tag == ParserResultType.NotParsed)
+                ShowTokenHelp();
         }
     }
 }

23116b776b9e7a3e1e92cd42b448ed24959112cb
Oleksii Holub <Oleksii.Holub@3shape.com>
2018-11-01 14:43:21 +0200

    Show unhandled exceptions to users

--- DiscordChatExporter.Gui/App.xaml.cs
+++ DiscordChatExporter.Gui/App.xaml.cs
@@ -1,6 +1,13 @@
-﻿namespace DiscordChatExporter.Gui
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace DiscordChatExporter.Gui
 {
     public partial class App
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            MessageBox.Show(args.Exception.ToString(), "Error occured", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }

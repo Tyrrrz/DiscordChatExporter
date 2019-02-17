bb98049df75a55607463da6fdda1b0a5cf17bcec
Alexey Golub <Tyrrrrrr@gmail.com>
2019-02-17 19:48:54 +0200

    Remove years from copyright

--- DiscordChatExporter.Gui/Properties/AssemblyInfo.cs
+++ DiscordChatExporter.Gui/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("DiscordChatExporter")]
 [assembly: AssemblyCompany("Tyrrrz")]
-[assembly: AssemblyCopyright("Copyright (c) 2017-2018 Alexey Golub")]
+[assembly: AssemblyCopyright("Copyright (c) Alexey Golub")]
 [assembly: AssemblyVersion("2.9.1")]
 [assembly: AssemblyFileVersion("2.9.1")]

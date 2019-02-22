a39749ae674f15d86a423fd6047077c598aab514
Oleksii Holub <Oleksii.Holub@3shape.com>
2019-02-22 18:05:52 +0200

    More grammar consistency

--- DiscordChatExporter.Core.Markdown/Internal/Grammar.cs
+++ DiscordChatExporter.Core.Markdown/Internal/Grammar.cs
@@ -9,34 +9,48 @@ namespace DiscordChatExporter.Core.Markdown.Internal
     // parsing as closely as possible, including its numerous flaws and inconsistencies
     internal static class Grammar
     {
-        // Parses the text contained within two instances of the given token
-        private static Parser<string> ContentWithinTokens(string token) =>
-            Parse.RegexMatch(new Regex(
-                $"{Regex.Escape(token)}" + // open token
-                "(.+?)" + // any non-empty content inside
-                $"{Regex.Escape(token)}" + // close token
-                $"(?=[^{Regex.Escape(token.Last().ToString())}]|$)", // followed by a different char than last or EOL
-                RegexOptions.Singleline)).Select(m => m.Groups[1].Value); // get 1st group value
+        // TODO: escape char \
 
         /* Formatting */
 
+        // May contain italics inside, so capture until the last double asterisk that isn't followed by an asterisk
         private static readonly Parser<Node> BoldFormattedNode =
-            ContentWithinTokens("**").Select(s => new FormattedNode(TextFormatting.Bold, BuildTree(s)));
+            Parse.RegexMatch(new Regex("\\*\\*(.+?)\\*\\*(?!\\*)", RegexOptions.Singleline))
+                .Select(m => m.Groups[1].Value)
+                .Select(s => new FormattedNode(TextFormatting.Bold, BuildTree(s)));
 
+        // TODO: *asd**
+        // TODO: ***s**a**b***
+        // TODO: one *two **three** __four__* _five_ ***six***
+        // Cannot have whitespace right after opening or right before closing token
         private static readonly Parser<Node> ItalicFormattedNode =
-            ContentWithinTokens("*").Select(s => new FormattedNode(TextFormatting.Italic, BuildTree(s)));
+            Parse.RegexMatch(new Regex("\\*(?!\\s)(.+?)(?<!\\s)\\*(?!\\*)", RegexOptions.Singleline))
+                .Select(m => m.Groups[1].Value)
+                .Select(s => new FormattedNode(TextFormatting.Italic, BuildTree(s)));
 
+        // Opening/closing token cannot come after/before word characters
         private static readonly Parser<Node> ItalicAltFormattedNode =
-            ContentWithinTokens("_").Select(s => new FormattedNode(TextFormatting.Italic, BuildTree(s)));
+            Parse.RegexMatch(new Regex("(?<!\\w)_(.+?)_(?!\\w)", RegexOptions.Singleline))
+                .Select(m => m.Groups[1].Value)
+                .Select(s => new FormattedNode(TextFormatting.Italic, BuildTree(s)));
 
+        // May contain italics inside, so capture until the last double underline that isn't followed by an underline
         private static readonly Parser<Node> UnderlineFormattedNode =
-            ContentWithinTokens("__").Select(s => new FormattedNode(TextFormatting.Underline, BuildTree(s)));
+            Parse.RegexMatch(new Regex("__(.+?)__(?!_)", RegexOptions.Singleline))
+                .Select(m => m.Groups[1].Value)
+                .Select(s => new FormattedNode(TextFormatting.Underline, BuildTree(s)));
 
+        // Strikethrough is safe
         private static readonly Parser<Node> StrikethroughFormattedNode =
-            ContentWithinTokens("~~").Select(s => new FormattedNode(TextFormatting.Strikethrough, BuildTree(s)));
+            Parse.RegexMatch(new Regex("~~(.+?)~~", RegexOptions.Singleline))
+                .Select(m => m.Groups[1].Value)
+                .Select(s => new FormattedNode(TextFormatting.Strikethrough, BuildTree(s)));
 
+        // Spoiler is safe
         private static readonly Parser<Node> SpoilerFormattedNode =
-            ContentWithinTokens("||").Select(s => new FormattedNode(TextFormatting.Spoiler, BuildTree(s)));
+            Parse.RegexMatch(new Regex("\\|\\|(.+?)\\|\\|", RegexOptions.Singleline))
+                .Select(m => m.Groups[1].Value)
+                .Select(s => new FormattedNode(TextFormatting.Spoiler, BuildTree(s)));
 
         private static readonly Parser<Node> AnyFormattedNode = BoldFormattedNode
             .Or(UnderlineFormattedNode).Or(ItalicFormattedNode).Or(ItalicAltFormattedNode) // italic should be after bold & underline
@@ -44,14 +58,16 @@ namespace DiscordChatExporter.Core.Markdown.Internal
 
         /* Code blocks */
 
+        // Can't have backticks inside and surrounding whitespace is trimmed
         private static readonly Parser<Node> InlineCodeBlockNode =
-            ContentWithinTokens("`").Select(s => new InlineCodeBlockNode(s));
+            Parse.RegexMatch(new Regex("`\\s*([^`]+?)\\s*`", RegexOptions.Singleline))
+                .Select(m => m.Groups[1].Value)
+                .Select(s => new InlineCodeBlockNode(s));
 
+        // The first word is a language identifier if it's the only word followed by a newline, the rest is code
         private static readonly Parser<Node> MultilineCodeBlockNode =
-            ContentWithinTokens("```").Select(s =>
-                Parse.RegexMatch(new Regex("^(?:(\\S*?)?(?:\\s*?\\n))?(.+)", RegexOptions.Singleline))
-                    .Select(m => new MultilineCodeBlockNode(m.Groups[1].Value, m.Groups[2].Value))
-                    .Parse(s));
+            Parse.RegexMatch(new Regex("```(?:(\\S*?)?(?:\\s*?\\n))?(.+)```", RegexOptions.Singleline))
+                .Select(m => new MultilineCodeBlockNode(m.Groups[1].Value, m.Groups[2].Value));
 
         private static readonly Parser<Node> AnyCodeBlockNode = MultilineCodeBlockNode.Or(InlineCodeBlockNode); // inline should be after multiline
 
@@ -82,6 +98,8 @@ namespace DiscordChatExporter.Core.Markdown.Internal
         private static readonly Parser<Node> EmojiNode = Parse.RegexMatch("<:(.+):(\\d+)>")
             .Select(m => new EmojiNode(m.Groups[2].Value, m.Groups[1].Value));
 
+        private static readonly Parser<Node> AnyEmojiNode = EmojiNode;
+
         /* Links */
 
         // TODO: check boundaries
@@ -94,16 +112,25 @@ namespace DiscordChatExporter.Core.Markdown.Internal
 
         private static readonly Parser<Node> AnyLinkNode = TitledLinkNode.Or(AutoLinkNode);
 
-        // Functional node is any node apart from plain text
-        private static readonly Parser<Node> FunctionalNode =
-            AnyFormattedNode.Or(AnyCodeBlockNode).Or(AnyMentionNode).Or(EmojiNode).Or(AnyLinkNode);
+        /* Functional (everything above) */
 
-        // Text node is created for text spans that don't match with any functional nodes
-        private static readonly Parser<Node> TextNode =
-            Parse.AnyChar.Except(FunctionalNode).AtLeastOnce().Text().Select(s => new TextNode(s));
+        private static readonly Parser<Node> AnyFunctionalNode =
+            AnyFormattedNode.Or(AnyCodeBlockNode).Or(AnyMentionNode).Or(AnyEmojiNode).Or(AnyLinkNode);
+
+        /* Text */
+
+        // Shrug is an exception and doesn't get formatted
+        private static readonly Parser<Node> ShrugTextNode =
+            Parse.String("¯\\_(ツ)_/¯").Text().Select(s => new TextNode(s));
+
+        // Fallback text node is for any node that is not functional
+        private static readonly Parser<Node> FallbackTextNode =
+            Parse.AnyChar.Except(AnyFunctionalNode).AtLeastOnce().Text().Select(s => new TextNode(s));
+
+        private static readonly Parser<Node> AnyTextNode = ShrugTextNode.Or(FallbackTextNode);
 
         // Any node
-        private static readonly Parser<Node> AnyNode = FunctionalNode.Or(TextNode);
+        private static readonly Parser<Node> AnyNode = AnyTextNode.Or(AnyFunctionalNode); // functional should be after text
 
         // Entry point
         public static IReadOnlyList<Node> BuildTree(string input) => AnyNode.Many().Parse(input).ToArray();

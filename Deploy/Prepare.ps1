d1d82f975b0953fe63879eae27404fe7c1021c1b
Alexey Golub <Tyrrrrrr@gmail.com>
2019-03-03 22:26:02 +0200

    Add license file to releasable artifacts

--- Deploy/Prepare.ps1
+++ Deploy/Prepare.ps1
@@ -2,10 +2,12 @@ New-Item "$PSScriptRoot\bin" -ItemType Directory -Force
 
 # GUI
 $files = @()
+$files += Get-Item -Path "$PSScriptRoot\..\License.txt"
 $files += Get-ChildItem -Path "$PSScriptRoot\..\DiscordChatExporter.Gui\bin\Release\*" -Include "*.exe", "*.dll", "*.config"
 $files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\DiscordChatExporter.zip" -Force
 
 # CLI
 $files = @()
+$files += Get-Item -Path "$PSScriptRoot\..\License.txt"
 $files += Get-ChildItem -Path "$PSScriptRoot\..\DiscordChatExporter.Cli\bin\Release\net461\*" -Include "*.exe", "*.dll", "*.config"
 $files | Compress-Archive -DestinationPath "$PSScriptRoot\bin\DiscordChatExporter.CLI.zip" -Force

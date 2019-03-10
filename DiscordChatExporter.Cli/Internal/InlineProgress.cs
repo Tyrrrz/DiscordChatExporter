1976e3ad0803df77ae2fc576f588800f477eb9f4
Alexey Golub <Tyrrrrrr@gmail.com>
2019-03-10 14:40:08 +0200

    [CLI] Don't write progress if output is redirected
    FIxes #155

--- DiscordChatExporter.Cli/Internal/InlineProgress.cs
+++ DiscordChatExporter.Cli/Internal/InlineProgress.cs
@@ -9,19 +9,31 @@ namespace DiscordChatExporter.Cli.Internal
 
         public InlineProgress()
         {
-            _posX = Console.CursorLeft;
-            _posY = Console.CursorTop;
+            // If output is not redirected - save initial cursor position
+            if (!Console.IsOutputRedirected)
+            {
+                _posX = Console.CursorLeft;
+                _posY = Console.CursorTop;
+            }
         }
 
         public void Report(double progress)
         {
-            Console.SetCursorPosition(_posX, _posY);
-            Console.WriteLine($"{progress:P1}");
+            // If output is not redirected - reset cursor position and write progress
+            if (!Console.IsOutputRedirected)
+            {
+                Console.SetCursorPosition(_posX, _posY);
+                Console.WriteLine($"{progress:P1}");
+            }
         }
 
         public void Dispose()
         {
-            Console.SetCursorPosition(_posX, _posY);
+            // If output is not redirected - reset cursor position
+            if (!Console.IsOutputRedirected)
+                Console.SetCursorPosition(_posX, _posY);
+
+            // Inform about completion
             Console.WriteLine("Completed ✓");
         }
     }

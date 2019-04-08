cad55362f9e0d304e8a0b2c7f4bc21a1920fd7fa
Oleksii Holub <Oleksii.Holub@3shape.com>
2019-04-08 19:00:46 +0300

    4x speed improvement via custom parser

---
+++ DiscordChatExporter.Core.Markdown/Internal/RegexParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DiscordChatExporter.Core.Markdown.Internal
+{
+    public class RegexParser<T>
+    {
+        public Regex Regex { get; }
+        public Func<Match, T> Transform { get; }
+
+        public RegexParser(Regex regex, Func<Match, T> transform)
+        {
+            Regex = regex;
+            Transform = transform;
+        }
+    }
+}
---
+++ DiscordChatExporter.Core.Markdown/Internal/TransformedMatch.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DiscordChatExporter.Core.Markdown.Internal
+{
+    public class TransformedMatch<T>
+    {
+        public Match Match { get; }
+
+        public T Value { get; }
+
+        public TransformedMatch(Match match, T value)
+        {
+            Match = match;
+            Value = value;
+        }
+    }
+}

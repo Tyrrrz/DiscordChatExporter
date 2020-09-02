48bbd937c0b25f463a1116232423d820f6883158
wyattscarpenter <wyattscarpenter@gmail.com>
2020-09-01 23:28:54 -0500

    change command to exportall
    it will also get DMs, unless -e is provided to exclude them

--- DiscordChatExporter.Cli/Commands/ExportAllCommand.cs
+++ DiscordChatExporter.Cli/Commands/ExportAllCommand.cs
@@ -7,11 +7,19 @@ using DiscordChatExporter.Domain.Utilities;
 
 namespace DiscordChatExporter.Cli.Commands
 {
-    [Command("exportallguilds", Description = "Export all channels within specified all guilds.")]
+    [Command("exportall", Description = "Export all direct messages and all channels within all guilds.")]
     public class ExportAllGuildsCommand : ExportMultipleCommandBase
     {
         public override async ValueTask ExecuteAsync(IConsole console)
         {
+            [CommandOption("exclude-dm", 'e', Description = "If this flag is present, direct messages will not be exported.")]
+            public bool ExcludeDMs { get; set; }
+
+            if(!ExcludeDMs){
+                var dmChannels = await GetDiscordClient().GetGuildChannelsAsync(Guild.DirectMessages.Id);
+                await ExportMultipleAsync(console, dmChannels);
+            }
+
             var guilds = await GetDiscordClient().GetUserGuildsAsync();
             foreach (var guild in guilds.OrderBy(g => g.Name))
             {

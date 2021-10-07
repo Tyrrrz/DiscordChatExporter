2f3e1659889d0e6176dc115dd5ab4b0522ad578f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2021-10-07 15:54:01 +0300

    Update NuGet packages

--- DiscordChatExporter.Cli/Utils/Extensions/ConsoleExtensions.cs
+++ DiscordChatExporter.Cli/Utils/Extensions/ConsoleExtensions.cs
@@ -7,29 +7,13 @@ namespace DiscordChatExporter.Cli.Utils.Extensions
 {
     internal static class ConsoleExtensions
     {
-        private class NoopExclusivityMode : IExclusivityMode
-        {
-            public T Run<T>(Func<T> func) => func();
-
-            public Task<T> Run<T>(Func<Task<T>> func) => func();
-        }
-
-        public static IAnsiConsole CreateAnsiConsole(this IConsole console)
-        {
-            // Don't require exclusivity in tests.
-            // Workaround for https://github.com/spectreconsole/spectre.console/issues/494
-            var exclusivityMode = console is FakeConsole
-                ? new NoopExclusivityMode()
-                : null;
-
-            return AnsiConsole.Create(new AnsiConsoleSettings
+        public static IAnsiConsole CreateAnsiConsole(this IConsole console) =>
+            AnsiConsole.Create(new AnsiConsoleSettings
             {
                 Ansi = AnsiSupport.Detect,
                 ColorSystem = ColorSystemSupport.Detect,
-                Out = new AnsiConsoleOutput(console.Output),
-                ExclusivityMode = exclusivityMode
+                Out = new AnsiConsoleOutput(console.Output)
             });
-        }
 
         public static Progress CreateProgressTicker(this IConsole console) => console
             .CreateAnsiConsole()

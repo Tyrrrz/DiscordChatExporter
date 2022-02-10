51cc132e5d3485de6d73f950c607b283e5145ec5
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-02-10 17:23:32 +0200

    Display 24-hour time pickers for appropriate cultures in GUI
    Related to #808

---
+++ DiscordChatExporter.Gui/Utils/Internationalization.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace DiscordChatExporter.Gui.Utils;
+
+internal static class Internationalization
+{
+    public static bool Is24Hours =>
+        string.IsNullOrWhiteSpace(CultureInfo.CurrentCulture.DateTimeFormat.AMDesignator) &&
+        string.IsNullOrWhiteSpace(CultureInfo.CurrentCulture.DateTimeFormat.PMDesignator);
+}

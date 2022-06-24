5755334492ecd36d1ab943761d94fb5eff009184
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-06-24 12:26:35 +0300

    Update war in Ukraine message in CLI

--- DiscordChatExporter.Cli/Commands/Base/ExportCommandBase.cs
+++ DiscordChatExporter.Cli/Commands/Base/ExportCommandBase.cs
@@ -204,8 +204,8 @@ public abstract class ExportCommandBase : TokenCommandBase
     {
         // War in Ukraine message
         console.Output.WriteLine("========================================================================");
-        console.Output.WriteLine("||   Ukraine is at war! Support my country in its fight for freedom   ||");
-        console.Output.WriteLine("||   Learn more: https://tyrrrz.me                                    ||");
+        console.Output.WriteLine("||   Ukraine is at war! Support my country in its fight for freedom~  ||");
+        console.Output.WriteLine("||   Learn more on my website: https://tyrrrz.me                      ||");
         console.Output.WriteLine("========================================================================");
         console.Output.WriteLine("");
 

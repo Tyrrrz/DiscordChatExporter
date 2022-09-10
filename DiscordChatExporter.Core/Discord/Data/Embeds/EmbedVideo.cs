c0e1539ab51318aed8034dfc29d23c4bd785e352
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-09-10 23:39:16 +0300

    Update DiscordChatExporter.Core/Discord/Data/Embeds/EmbedVideo.cs

--- DiscordChatExporter.Core/Discord/Data/Embeds/EmbedVideo.cs
+++ DiscordChatExporter.Core/Discord/Data/Embeds/EmbedVideo.cs
@@ -6,16 +6,16 @@ namespace DiscordChatExporter.Core.Discord.Data.Embeds;
 // https://discord.com/developers/docs/resources/channel#embed-object-embed-video-structure
 public record EmbedVideo(
     string? Url,
+    string? ProxyUrl
     int? Width,
-    int? Height,
-    string? ProxyUrl)
+    int? Height)
 {
     public static EmbedVideo Parse(JsonElement json)
     {
         var url = json.GetPropertyOrNull("url")?.GetNonWhiteSpaceStringOrNull();
+        var proxyUrl = json.GetPropertyOrNull("proxy_url")?.GetNonWhiteSpaceStringOrNull();
         var width = json.GetPropertyOrNull("width")?.GetInt32OrNull();
         var height = json.GetPropertyOrNull("height")?.GetInt32OrNull();
-        var proxyUrl = json.GetPropertyOrNull("proxy_url")?.GetNonWhiteSpaceStringOrNull();
 
         return new EmbedVideo(url, width, height, proxyUrl);
     }

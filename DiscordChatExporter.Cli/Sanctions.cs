001ea7e495f118a03d2766e934dc4a3f992f9b4d
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-11-01 18:42:42 +0200

    Add region-based sanctions

---
+++ DiscordChatExporter.Cli/Sanctions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace DiscordChatExporter.Cli;
+
+public static class Sanctions
+{
+    [ModuleInitializer]
+    internal static void Verify()
+    {
+        var isSkipped = string.Equals(
+            Environment.GetEnvironmentVariable("RUSNI"),
+            "PYZDA",
+            StringComparison.OrdinalIgnoreCase
+        );
+
+        if (isSkipped)
+            return;
+
+        var isSanctioned = new[]
+        {
+            CultureInfo.CurrentCulture,
+            CultureInfo.CurrentUICulture,
+            CultureInfo.InstalledUICulture,
+            CultureInfo.DefaultThreadCurrentCulture,
+            CultureInfo.DefaultThreadCurrentUICulture
+        }.Any(c =>
+            c is not null && (
+                c.Name.Contains("-ru", StringComparison.OrdinalIgnoreCase) ||
+                c.Name.Contains("-by", StringComparison.OrdinalIgnoreCase)
+            )
+        );
+
+        if (!isSanctioned)
+            return;
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Error.WriteLine(
+            "You cannot use this software on the territory of a terrorist state. " +
+            "Set the environment variable `RUSNI=PYZDA` if you wish to override this check."
+        );
+
+        Console.ResetColor();
+
+        Environment.Exit(0xFACC);
+    }
+}

9ebd3905f3570bc21fe271ab195c1f451e3aa0c5
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-01-27 04:10:50 +0200

    Update DiscordChatExporter.Gui/App.xaml.cs

--- DiscordChatExporter.Gui/App.xaml.cs
+++ DiscordChatExporter.Gui/App.xaml.cs
@@ -17,7 +17,7 @@ public partial class App
 
     public static string GitHubProjectUrl { get; } = "https://github.com/Tyrrrz/DiscordChatExporter";
 
-    public static string GitHubProjectDocsUrl { get; } = GitHubProjectUrl + "/tree/master/.docs";
+    public static string GitHubProjectDocsUrl { get; } = GitHubProjectUrl + "/blob/master/.docs";
 }
 
 public partial class App

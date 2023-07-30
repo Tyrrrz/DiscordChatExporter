b224fca6c0499778dc20896342390c3aeb6ce169
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-07-30 14:54:15 +0300

    `ToHex()` should be uppercase by default

--- DiscordChatExporter.Core/Utils/Extensions/BinaryExtensions.cs
+++ DiscordChatExporter.Core/Utils/Extensions/BinaryExtensions.cs
@@ -4,12 +4,16 @@ namespace DiscordChatExporter.Core.Utils.Extensions;
 
 public static class BinaryExtensions
 {
-    public static string ToHex(this byte[] data)
+    public static string ToHex(this byte[] data, bool isUpperCase = true)
     {
         var buffer = new StringBuilder(2 * data.Length);
 
         foreach (var b in data)
-            buffer.Append(b.ToString("x2"));
+        {
+            buffer.Append(
+                b.ToString(isUpperCase ? "X2" : "x2")
+            );
+        }
 
         return buffer.ToString();
     }

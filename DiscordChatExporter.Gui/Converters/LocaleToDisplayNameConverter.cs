6a11ae2398aaeffc4186ac9ae328877a3b524f51
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-09-06 20:52:43 +0300

    asd

---
+++ DiscordChatExporter.Gui/Converters/LocaleToDisplayNameConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DiscordChatExporter.Gui.Converters;
+
+[ValueConversion(typeof(string), typeof(string))]
+public class LocaleToDisplayNameConverter : IValueConverter
+{
+    public static LocaleToDisplayNameConverter Instance { get; } = new();
+
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is string locale ? CultureInfo.GetCultureInfo(locale).DisplayName : null;
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture
+    ) => throw new NotSupportedException();
+}

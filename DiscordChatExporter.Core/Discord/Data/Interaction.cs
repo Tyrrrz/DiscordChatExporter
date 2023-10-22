7b7df564ed0c48127b5c05c820fc9a17c49060ba
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-10-22 19:22:51 +0300

    Allow empty names in interactions

--- DiscordChatExporter.Core/Discord/Data/Interaction.cs
+++ DiscordChatExporter.Core/Discord/Data/Interaction.cs
@@ -10,7 +10,7 @@ public record Interaction(Snowflake Id, string Name, User User)
     public static Interaction Parse(JsonElement json)
     {
         var id = json.GetProperty("id").GetNonWhiteSpaceString().Pipe(Snowflake.Parse);
-        var name = json.GetProperty("name").GetNonWhiteSpaceString();
+        var name = json.GetProperty("name").GetNonNullString(); // may be empty, but not null
         var user = json.GetProperty("user").Pipe(User.Parse);
 
         return new Interaction(id, name, user);

5397b0db7d7629921f7382b42c854e15e8192adc
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-16 21:12:41 +0200

    Fix formatting

--- DiscordChatExporter.Core/Exceptions/DiscordChatExporterException.cs
+++ DiscordChatExporter.Core/Exceptions/DiscordChatExporterException.cs
@@ -6,7 +6,11 @@ public class DiscordChatExporterException : Exception
 {
     public bool IsFatal { get; }
 
-    public DiscordChatExporterException(string message, bool isFatal = false, Exception? innerException = null)
+    public DiscordChatExporterException(
+        string message,
+        bool isFatal = false,
+        Exception? innerException = null
+    )
         : base(message, innerException)
     {
         IsFatal = isFatal;

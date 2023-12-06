54dcf39981d46ac2d358a3bea1b631e08722f671
Paul Bobik <paul.bobik@gmail.com>
2023-12-06 01:14:59 +0100

    Added "%r" placeholder to wrap parts of the path. Wrapped parts will be set recursively. #1123. e.g.: "\%G\%r%P – %T%r\%p – %C.html"

--- DiscordChatExporter.Core/Exporting/ExportRequest.cs
+++ DiscordChatExporter.Core/Exporting/ExportRequest.cs
@@ -153,8 +153,23 @@ public partial class ExportRequest
         Channel channel,
         Snowflake? after,
         Snowflake? before
-    ) =>
-        Regex.Replace(
+    )
+    {
+        var recursivePattern = Regex.Match(path, "%r(%.*)%r");
+        if (
+            channel.Parent?.Parent != null
+            && recursivePattern.Success
+            && recursivePattern.Groups.Count > 1
+        )
+        {
+            var groupValue = recursivePattern.Groups[1].Value;
+            path = path.Replace(
+                recursivePattern.Value,
+                FormatPath(groupValue, guild, channel.Parent, after, before)
+            );
+        }
+
+        return Regex.Replace(
             path,
             "%.",
             m =>
@@ -187,10 +202,12 @@ public partial class ExportRequest
                                 .ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
 
                         "%%" => "%",
+                        "%r" => string.Empty,
                         _ => m.Value
                     }
                 )
         );
+    }
 
     private static string GetOutputBaseFilePath(
         Guild guild,

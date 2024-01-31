7f17ec0575d365fbcf6b79f3253f817e89857520
Philippe Vaucher <philippe.vaucher@gmail.com>
2024-01-31 09:37:52 +0100

    Add comment explaining docker-entrypoint's workflow

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# If we are root, ensure the files in /out are writable
+# by the dce user and restart the process as the dce user
 if [ "$(id -u)" = '0' ]; then
   chown -R dce:dce /out
   exec su-exec dce "$0" "$@"

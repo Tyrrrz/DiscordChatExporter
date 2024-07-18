42e6de359e3b292fa85e0f2f6dfe912f06f64891
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-07-19 02:00:47 +0300

    Invert `WatchProperty(...)` methods' `watchInitialValue`

--- DiscordChatExporter.Gui/App.axaml.cs
+++ DiscordChatExporter.Gui/App.axaml.cs
@@ -65,8 +65,7 @@ public class App : Application, IDisposable
                     };
 
                     InitializeTheme();
-                },
-                false
+                }
             )
         );
     }

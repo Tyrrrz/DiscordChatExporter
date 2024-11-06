d2d2735561ac2a3686a24a5b5ecbc246f1f39b38
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-11-06 19:20:14 +0200

    Simplify the `Emoji` class

--- DiscordChatExporter.Core/Discord/Data/Emoji.cs
+++ DiscordChatExporter.Core/Discord/Data/Emoji.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text.Json;
+﻿using System.Text.Json;
 using DiscordChatExporter.Core.Discord.Data.Common;
 using DiscordChatExporter.Core.Utils;
 using DiscordChatExporter.Core.Utils.Extensions;
@@ -13,34 +12,22 @@ public partial record Emoji(
     Snowflake? Id,
     // Name of a custom emoji (e.g. LUL) or actual representation of a standard emoji (e.g. 🙂)
     string Name,
-    bool IsAnimated,
-    string ImageUrl
+    bool IsAnimated
 )
 {
-    public Emoji(Snowflake? id, string name, bool isAnimated)
-        : this(id, name, isAnimated, GetImageUrl(id, name, isAnimated)) { }
-
-    public bool IsCustomEmoji => Id is not null;
+    public bool IsCustomEmoji { get; } = Id is not null;
 
     // Name of a custom emoji (e.g. LUL) or name of a standard emoji (e.g. slight_smile)
-    public string Code => IsCustomEmoji ? Name : EmojiIndex.TryGetCode(Name) ?? Name;
+    public string Code { get; } = Id is not null ? Name : EmojiIndex.TryGetCode(Name) ?? Name;
+
+    public string ImageUrl { get; } =
+        Id is not null
+            ? ImageCdn.GetCustomEmojiUrl(Id.Value, IsAnimated)
+            : ImageCdn.GetStandardEmojiUrl(Name);
 }
 
 public partial record Emoji
 {
-    private static string GetImageUrl(Snowflake? id, string? name, bool isAnimated)
-    {
-        // Custom emoji
-        if (id is not null)
-            return ImageCdn.GetCustomEmojiUrl(id.Value, isAnimated);
-
-        // Standard emoji
-        if (!string.IsNullOrWhiteSpace(name))
-            return ImageCdn.GetStandardEmojiUrl(name);
-
-        throw new InvalidOperationException("Either the emoji ID or name should be provided.");
-    }
-
     public static Emoji Parse(JsonElement json)
     {
         var id = json.GetPropertyOrNull("id")

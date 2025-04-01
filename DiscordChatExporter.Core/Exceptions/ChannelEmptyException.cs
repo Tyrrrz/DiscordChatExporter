fe6ba1e8f545cc8b16bf00dbaa25ce1b613bede6
Leonardo Mosquera <ldmosquera@gmail.com>
2025-04-01 14:37:55 -0300

    Fix one more formatting issue

--- DiscordChatExporter.Core/Exceptions/ChannelEmptyException.cs
+++ DiscordChatExporter.Core/Exceptions/ChannelEmptyException.cs
@@ -5,5 +5,4 @@ namespace DiscordChatExporter.Core.Exceptions;
 // Thrown when there is circumstancially no message to export with given parameters,
 // though it should not be treated as a runtime error; simply warn instead
 public class ChannelEmptyException(string message)
-    : DiscordChatExporterException(message, false, null)
-{ }
+    : DiscordChatExporterException(message, false, null) { }

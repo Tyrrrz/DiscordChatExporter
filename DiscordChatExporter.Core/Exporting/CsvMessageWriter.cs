a4d17c8c051d6bd0882e5fef7d131d703b2eb716
farley128 <Markfarley6120@hotmail.com>
2025-12-26 16:18:41 -0500

    Re-add a few lines to the .csv exporter that were accidentally deleted

--- DiscordChatExporter.Core/Exporting/CsvMessageWriter.cs
+++ DiscordChatExporter.Core/Exporting/CsvMessageWriter.cs
@@ -92,6 +92,11 @@ internal partial class CsvMessageWriter(Stream stream, ExportContext context)
         {
             await _writer.WriteAsync(CsvEncode(message.GetFallbackContent()));
         }
+        {
+            await _writer.WriteAsync(
+                CsvEncode(await FormatMarkdownAsync(message.Content, cancellationToken))
+            );
+        }
 
         await _writer.WriteAsync(',');
 
